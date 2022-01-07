963888b31724e84318f6ec1d762c5f610eabfa88
khuongtm <khuongtranuet@gmail.com>
2022-01-07 16:18:02 +0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 const mysql = require('mysql');
+const { off } = require("process");
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
 });
 
 let usernames = {};
+let roomid = {};
 
 io.use((socket, next) => {
     const username = socket.handshake.auth.username;
@@ -33,6 +35,7 @@ io.use((socket, next) => {
                 }
                 socket.username = username;
                 usernames[username] = username;
+                roomid[socket.id] = username;
                 next();
             }
         });
@@ -45,13 +48,14 @@ io.on('connection', (socket) => {
     console.log('Một người dùng đã kết nối');
 
     socket.emit('login', usernames, socket.username);
-    io.sockets.emit('notification', 'SERVER', socket.username + ' đã tham gia phòng chat');
+    io.sockets.emit('notification', 'SERVER', socket.username + ' đã tham gia kênh thế giới');
     io.sockets.emit('update_users', usernames);
+    io.sockets.emit('listroom', roomid);
 
     socket.on('add_user', (username) => {
         socket.username = username;
         usernames[username] = username;
-        io.sockets.emit('notification', 'SERVER', username + ' đã tham gia phòng chat');
+        io.sockets.emit('notification', 'SERVER', username + ' đã tham gia kênh thế giới');
 
         io.sockets.emit('update_users', usernames);
     });
@@ -63,11 +67,13 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Người dùng đã ngắt kết nối');
         delete usernames[socket.username];
+        delete roomid[socket.id];
         io.sockets.emit('update_users', usernames);
-        socket.broadcast.emit('notification', 'SERVER', socket.username + ' đã rời phòng chat');
+        io.sockets.emit('listroom', roomid);
+        socket.broadcast.emit('notification', 'SERVER', socket.username + ' đã ngắt kết nối');
     });
 
-    socket.on('base64 file', function (msg) {
+    socket.on('base64 file', (msg) => {
         console.log('received base64 file from');
         // socket.username = msg.username;
         io.sockets.emit('base64 file',
@@ -78,6 +84,40 @@ io.on('connection', (socket) => {
             }
     
         );
+    });
+
+    socket.on('base64 file room', (msg, idRoom) => {
+        console.log('received base64 file from');
+        // socket.username = msg.username;
+        io.sockets.to(idRoom).emit('base64 file room',
+            {
+              username: socket.username,
+              file: msg.file,
+              fileName: msg.fileName
+            }
+    
+        );
+    });
+
+    socket.on('create_room', (room) => {
+        let list_room = [];
+        socket.join(room);
+        socket.Room = room;
+        console.log(socket.rooms);
+        let checkroom = Object.values(roomid).filter((i => i === room))
+        if(!checkroom.length) {
+            roomid[room] = room;
+        }
+        // for(r of io.sockets.adapter.rooms) {
+            // console.log(r);
+            // roomid[room] = room;
+        // }
+        io.sockets.emit('listroom', roomid);
+        socket.emit('nameroom', room);
+    });
+
+    socket.on('send_room', (msg, idRoom) => {
+        io.sockets.to(idRoom).emit('messagetoroom', socket.username, msg);
     });
 });
 
